373aff89dc3fd6957bf6e8f547807420c91eb020
Yvan <yndayirukiy@gmail.com>
2026-07-29 14:41:10 -0400

    Ajout du GestionnaireFichierCSV pour le chargement des documents et la gestion des erreurs CSV

--- src/ca/bibliotheque/Main.java
+++ src/ca/bibliotheque/Main.java
@@ -1,4 +1,20 @@
 package ca.bibliotheque;
 
+import ca.bibliotheque.model.Document;
+import ca.bibliotheque.service.GestionnaireFichierCSV;
+
+import java.util.List;
+
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("=== CHARGEMENT DE LA BIBLIOTHÈQUE DEPUIS LE CSV ===\n");
+
+        // Chargement du fichier CSV
+        List<Document> bibliotheque = GestionnaireFichierCSV.chargerDocuments("donnees_bibliotheque.csv");
+
+        System.out.println("--- LISTE DES DOCUMENTS CHARGÉS ---");
+        for (Document doc : bibliotheque) {
+            System.out.println(doc);
+        }
+    }
 }
