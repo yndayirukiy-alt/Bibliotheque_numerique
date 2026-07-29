a0b220d208d16b2e137f6d34a1e5b5840443eb2d
Yvan <yndayirukiy@gmail.com>
2026-07-29 13:42:56 -0400

    Creation de DocumentIndisponibleException pour la gestion des emprunts

---
+++ src/ca/bibliotheque/exception/DocumentIndisponibleException.java
@@ -0,0 +1,7 @@
+package ca.bibliotheque.exception;
+
+public class DocumentIndisponibleException extends Exception {
+    public DocumentIndisponibleException(String message) {
+        super(message);
+    }
+}
