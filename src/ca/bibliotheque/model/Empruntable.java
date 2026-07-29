e6172da3ec50fff70bff3a318290d84b03bfa8bc
Yvan <yndayirukiy@gmail.com>
2026-07-29 13:53:09 -0400

    Polymorphisme & Interface : toutes les classes implémentent Empruntable

--- src/ca/bibliotheque/model/Empruntable.java
+++ src/ca/bibliotheque/model/Empruntable.java
@@ -1,5 +1,7 @@
 package ca.bibliotheque.model;
 
+import ca.bibliotheque.exception.DocumentIndisponibleException;
+
 public interface Empruntable {
 
         void emprunter() throws DocumentIndisponibleException;
